285419a5a8201c6f899caf30c5c70ed0c9894dcf
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-09-13 20:30:50 -0400

    Create app js file. Created event listener for form group

---
+++ app.js
@@ -0,0 +1,9 @@
+document.getElementById('mortgage_form').addEventListener('submit', calculateMortgage);
+
+function calculateMortgage(event){
+
+    console.log('calculating mortgage')
+
+    event.preventDefault();
+
+}
