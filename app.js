1b4b2c847d4c7a72d51d19203a7c132651628f72
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-09-26 14:50:51 -0400

    Creating initial calculations for mortgage interest rates

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
 document.getElementById('mortgage_form').addEventListener('submit', calculateMortgage);
 
-function calculateMortgage(event){
+function calculateMortgage(event) {
 
-    console.log('calculating mortgage')
+    console.log('calculating mortgage');
+
+    // Input values and variables
+
+    const mortgageAmount = document.getElementById('mortgage_amount');
+    const interestRate = document.getElementById('interest_rate');
+    const yearsMortgage = document.getElementById('years');
+
+    // Output values
+
+    const totalMortgageCost = document.getElementById('totalmortgage_cost');
+    const monthlyRate = document.getElementById('monthly_rate');
+
+    // Integers to strings.
+
+    const principalMortgage = parseFloat(mortgageAmount.value);
+    const interestCalculation = parseFloat(interestRate.value / 100);
+    const monthCalculation = parseFloat(yearsMortgage.value * 12);
+
+    // Calculating interest Rate;
+
+    const interestRateCalc = interestCalculation/12
+
+    // Calculating mortgage principal times interest rate;
+
+    const calculationOne = principalMortgage * interestRateCalc;
+    console.log(calculationOne);
+
+    // Calculating rate plus 1 in numerator 
+    const calculationTwo = 1 + (interestCalculation/12);
+    console.log(calculationTwo);
+
+    // Calculating rate the power in the Exponent - Numerator - Denomitor uses same
+    const calculationThree = Math.pow(calculationTwo, monthCalculation);
+    console.log(calculationThree);
+
+    //
 
     event.preventDefault();
 
 }
+
